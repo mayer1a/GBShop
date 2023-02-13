77db322de89711bf2a69449ae7308846ed7d595e
Artem V. Mayer <mayer1art@gmail.com>
2023-02-13 19:56:09 +0700

    add RequestFactory to send requests to the network

---
+++ GBShop/GBShop/App/BusinessLogic/Network/RequestFactory.swift
@@ -0,0 +1,36 @@
+//
+//  RequestFactory.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 13.02.2023.
+//
+
+import Alamofire
+
+class RequestFactory {
+
+    // MARK: - Properties
+
+    lazy var commonSession: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpShouldSetCookies = false
+        configuration.headers = .default
+
+        let manager = Session(configuration: configuration)
+        return manager
+    }()
+
+    let sessionQueue = DispatchQueue.global(qos: .utility)
+
+    // MARK: - Functions
+
+    func makeErrorParser() -> AbstractErrorParser {
+        return ErrorParser()
+    }
+
+    func makeAuthRequestFatory() -> AuthRequestFactory {
+        let errorParser = makeErrorParser()
+        return Auth(errorParser: errorParser, sessionManager: commonSession, queue: sessionQueue)
+    }
+
+}
