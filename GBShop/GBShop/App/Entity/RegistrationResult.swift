c4d6bebd8220e3e5b354d149b15b8b1749de8aa7
Artem V. Mayer <mayer1art@gmail.com>
2023-02-14 00:19:56 +0700

    add RegistrationResult model

---
+++ GBShop/GBShop/App/Entity/RegistrationResult.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationResult.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 14.02.2023.
+//
+
+import Foundation
+
+struct RegistrationResult: Codable {
+
+    // MARK: - Properties
+
+    let result: Int
+    let userMessage: String
+}
