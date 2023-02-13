35fb778dff4347b1cab826ba889a4ce75a619558
Artem V. Mayer <mayer1art@gmail.com>
2023-02-13 19:53:42 +0700

    add Auth class implementing AuthRequestFactory

---
+++ GBShop/GBShop/App/BusinessLogic/Network/Auth.swift
@@ -0,0 +1,70 @@
+//
+//  Auth.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 13.02.2023.
+//
+
+import Alamofire
+
+class Auth: AbstractRequestFactory {
+
+    // MARK: - Properties
+
+    let errorParser: AbstractErrorParser
+    let sessionManager: Session
+    let queue: DispatchQueue
+    let baseUrl = URL(string: "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/")!
+
+    // MARK: - Constructions
+
+    init(
+        errorParser: AbstractErrorParser,
+        sessionManager: Session,
+        queue: DispatchQueue = .global(qos: .utility)
+    ) {
+        self.errorParser = errorParser
+        self.sessionManager = sessionManager
+        self.queue = queue
+    }
+}
+
+// MARK: - Extensions
+
+extension Auth {
+
+    // MARK: - RequestRouter
+
+    struct Login: RequestRouter {
+
+        // MARK: - Properties
+
+        let baseUrl: URL
+        let method: HTTPMethod = .get
+        let path: String = "login.json"
+
+        let login: String
+        let password: String
+
+        var parameters: Parameters? {
+            return [
+                "username": login,
+                "password": password
+            ]
+        }
+    }
+}
+
+extension Auth: AuthRequestFactory {
+
+    // MARK: - Functions
+
+    func login(
+        userName: String,
+        password: String,
+        completionHandler: @escaping (AFDataResponse<LoginResult>) -> Void
+    ) {
+        let requestModel = Login(baseUrl: baseUrl, login: userName, password: password)
+        self.request(request: requestModel, completionHandler: completionHandler)
+    }
+}
