443609973f0b4acd9345182e81c5a85e3e73b963
Artem V. Mayer <mayer1art@gmail.com>
2023-02-14 11:47:57 +0700

    add LogoutResult model

---
+++ GBShop/GBShop/App/Entity/LogoutResult.swift
@@ -0,0 +1,15 @@
+//
+//  LogoutResult.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 14.02.2023.
+//
+
+import Foundation
+
+struct LogoutResult: Codable {
+
+    // MARK: - Properties
+
+    let result: Int
+}
