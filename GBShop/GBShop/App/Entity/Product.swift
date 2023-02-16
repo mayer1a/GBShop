81c9c41ef195ae40fcd348136680bc180881845d
Artem V. Mayer <mayer1art@gmail.com>
2023-02-16 15:27:26 +0700

    add Product model

---
+++ GBShop/GBShop/App/Entity/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 16.02.2023.
+//
+
+import Foundation
+
+struct Product {
+    let id: Int
+    let productName: String
+    let price: Int
+}
