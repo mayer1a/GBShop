086f3812a154117400cb3e41d7698ce9f9b95aec
Artem V. Mayer <mayer1art@gmail.com>
2023-02-27 13:37:52 +0700

    add ApproveReviewResult model

---
+++ GBShop/GBShop/App/Entity/ResultsEntity/ApproveReviewResult.swift
@@ -0,0 +1,17 @@
+//
+//  ApproveReviewResult.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 27.02.2023.
+//
+
+import Foundation
+
+// MARK: - ApproveReviewResult
+
+struct ApproveReviewResult: Codable {
+
+    // MARK: - Properties
+
+    let result: Int
+}
