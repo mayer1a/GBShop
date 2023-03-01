9fc5cbf2b4e954108ed1ab0609785f368daa0b23
Artem V. Mayer <mayer1art@gmail.com>
2023-03-01 13:35:34 +0700

    add Gender enum model

---
+++ GBShop/GBShop/App/Entity/Gender.swift
@@ -0,0 +1,16 @@
+//
+//  Gender.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 01.03.2023.
+//
+
+import Foundation
+
+// MARK: - Gender
+
+enum Gender: String, Codable {
+    case man = "m"
+    case woman = "w"
+    case indeterminate = "ind"
+}
