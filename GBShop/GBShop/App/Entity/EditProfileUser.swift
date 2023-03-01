90d6735b7aa9bf0657f70dbdc60cec76e450c5aa
Artem V. Mayer <mayer1art@gmail.com>
2023-03-01 13:50:37 +0700

    add EditProfileUser model

---
+++ GBShop/GBShop/App/Entity/EditProfileUser.swift
@@ -0,0 +1,35 @@
+//
+//  EditProfileUser.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 01.03.2023.
+//
+
+import Foundation
+
+// MARK: - User
+
+struct EditProfileUser: Codable {
+
+    // MARK: - CodingKeys
+
+    enum CodingKeys: String, CodingKey {
+        case id = "user_id"
+        case username
+        case password
+        case email
+        case creditCard = "credit_card"
+        case gender
+        case bio
+    }
+
+    // MARK: - Properties
+
+    let id: Int
+    let username: String
+    let password: String
+    let email: String
+    let creditCard: String
+    let gender: Gender
+    let bio: String
+}
