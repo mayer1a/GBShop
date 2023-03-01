bf9d0c3ef9e7f20e32d71e62751158dd6f5feeff
Artem V. Mayer <mayer1art@gmail.com>
2023-03-01 22:05:14 +0700

    add AddProductResult model

---
+++ GBShop/GBShop/App/Entity/ResultsEntity/AddProductResult.swift
@@ -0,0 +1,17 @@
+//
+//  AddProductResult.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 01.03.2023.
+//
+
+import Foundation
+
+// MARK: - AddProductResult
+
+struct AddProductResult: Codable {
+
+    // MARK: - Properties
+
+    let result: Int
+}
