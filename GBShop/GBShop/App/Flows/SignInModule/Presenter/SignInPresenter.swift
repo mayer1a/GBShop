90b2f37114b2fe5fb229a1df2f803c61f02bdb71
Artem V. Mayer <mayer1art@gmail.com>
2023-03-09 19:54:23 +0700

    add necessary properties to protocols, keyboard observers and update signIn method

--- GBShop/GBShop/App/Flows/SignInModule/Presenter/SignInPresenter.swift
+++ GBShop/GBShop/App/Flows/SignInModule/Presenter/SignInPresenter.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 protocol SignInViewProtocol: AnyObject {
+    var scrollView: UIScrollView { get }
+
     func startLoadingSpinner()
     func stopLoadingSpinner()
     func signInFailure()
     func removeWarning()
+    func getSignInButtonFrame() -> CGRect
+    func getSafeAreaLayoutFrame() -> CGRect
 }
 
 protocol SignInPresenterProtocol: AnyObject {
     init(view: SignInViewProtocol, requestFactory: SignInRequestFactory)
 
-    func signIn(username: String, password: String)
+    func signIn(username: String?, password: String?)
     func signUpButtonTapped()
     func inputFieldsTapped()
 
@@ -37,23 +41,67 @@ final class SignInPresenter: SignInPresenterProtocol {
     init(view: SignInViewProtocol, requestFactory: SignInRequestFactory) {
         self.view = view
         self.requestFactory = requestFactory
+
+        addKeyboardObservers()
+    }
+
+    deinit {
+        removeKeyboardObservers()
+    }
+
+    // MARK: - Private functions
+
+    private func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(moveFrameUp),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(moveFrameBack),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+
+    private func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
     }
 
     // MARK: - Functions
 
-    func signIn(username: String, password: String) {
+    func signIn(username: String?, password: String?) {
+        guard
+            let username,
+            let password
+        else {
+            view?.signInFailure()
+            return
+        }
+
         view?.startLoadingSpinner()
 
         requestFactory?.login(userName: username, password: password) { [weak self] response in
-            switch response.result {
-            case .success(let user):
-                self?.user = user
-                // TODO: Go to Main screen through the Coordinator
-            case .failure(_):
-                self?.view?.signInFailure()
+            DispatchQueue.main.async {
+                switch response.result {
+                case .success(let user):
+                    self?.user = user
+                    // TODO: Go to Main screen through the Coordinator
+                case .failure(_):
+                    self?.view?.signInFailure()
+                }
+                
+                self?.view?.stopLoadingSpinner()
             }
-
-            self?.view?.stopLoadingSpinner()
         }
     }
 
