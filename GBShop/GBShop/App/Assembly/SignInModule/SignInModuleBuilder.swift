d3728fd1801893620efd5f684b4f129506cb12bc
Artem V. Mayer <mayer1art@gmail.com>
2023-03-09 21:45:23 +0700

    add plug-design mainViewController for test

--- GBShop/GBShop/App/Assembly/SignInModule/SignInModuleBuilder.swift
+++ GBShop/GBShop/App/Assembly/SignInModule/SignInModuleBuilder.swift
@@ -9,7 +9,7 @@ import UIKit
 
 protocol ModuleBuilderProtocol {
     static func createSignInModule() -> UIViewController
-    static func createMainModule(with user: SignInResult) -> UIViewController
+    static func createMainModule(with user: User) -> UIViewController
     static func createSignUpModule() -> UIViewController
 }
 
@@ -26,9 +26,26 @@ final class ModuleBuilder: ModuleBuilderProtocol {
         return signInView
     }
 
-    static func createMainModule(with user: SignInResult) -> UIViewController {
+    static func createMainModule(with user: User) -> UIViewController {
         // TODO: add MainModule assembly components when ready
-        return UIViewController()
+        let welcomeLabel = UILabel()
+        welcomeLabel.text = "Добро пожаловать, \(user.name) \(user.lastname)!"
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.backgroundColor = .systemBackground
+        welcomeLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .systemBackground
+        viewController.view.addSubview(welcomeLabel)
+
+        NSLayoutConstraint.activate([
+            welcomeLabel.leftAnchor.constraint(equalTo: viewController.view.leftAnchor, constant: 20),
+            welcomeLabel.rightAnchor.constraint(equalTo: viewController.view.rightAnchor, constant: -20),
+            welcomeLabel.topAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.topAnchor)
+        ])
+
+        return viewController
     }
 
     static func createSignUpModule() -> UIViewController {
