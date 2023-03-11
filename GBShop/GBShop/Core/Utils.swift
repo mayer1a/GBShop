f8ae9b8e46cdaa5912a888a2f84c903b4e1b4183
Artem V. Mayer <mayer1art@gmail.com>
2023-03-12 00:09:48 +0700

    add Utils with app Constants

---
+++ GBShop/GBShop/Core/Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 11.03.2023.
+//
+
+import Foundation
+
+// MARK: - App Constants
+
+struct Constants {
+    static let passwordMinLength = 8
+}
