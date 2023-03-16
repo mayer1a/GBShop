70db7b45c9bc12ed223201e2aeb6e45511394714
Artem V. Mayer <mayer1art@gmail.com>
2023-03-16 21:09:27 +0700

    add modelFactory methods

--- GBShop/GBShop/Core/ModelFactory/UserModelFactory.swift
+++ GBShop/GBShop/Core/ModelFactory/UserModelFactory.swift
@@ -48,4 +48,41 @@ struct UserModelFactory {
             gender: gender,
             bio: bio)
     }
+
+    func construct(from model: SignUpRawModel, with user: User) -> EditProfile? {
+        guard
+            let name = model.name == user.name ? "" : model.name,
+            let lastname = model.lastname == user.lastname ? "" : model.lastname,
+            let username = model.username == user.username ? "" : model.username,
+            let email = model.email == user.email ? "" : model.email,
+            let cardNumber = model.creditCard == user.creditCard ? "" : model.creditCard,
+            let genderString = model.gender,
+            let gender = Gender(rawValue: genderString),
+            let bio = model.bio == user.bio ? "" : model.bio
+        else {
+            return nil
+        }
+
+        return EditProfile(
+            userId: user.id,
+            name: name,
+            lastname: lastname,
+            username: username,
+            email: email,
+            creditCard: cardNumber,
+            gender: gender,
+            bio: bio)
+    }
+
+    func construct(from editModel: EditProfile, with id: Int) -> User {
+        User(
+            id: id,
+            username: editModel.username,
+            name: editModel.name,
+            email: editModel.email,
+            creditCard: editModel.creditCard,
+            lastname: editModel.lastname,
+            gender: editModel.gender,
+            bio: editModel.bio)
+    }
 }
