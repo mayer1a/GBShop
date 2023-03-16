e4f577b8a22a1592e7d6921720d03b7bf87707a0
Artem V. Mayer <mayer1art@gmail.com>
2023-03-16 19:16:36 +0700

    add model factory

--- GBShop/GBShop/Core/ModelFactory/UserModelFactory.swift
+++ GBShop/GBShop/Core/ModelFactory/UserModelFactory.swift
@@ -22,4 +22,30 @@ struct UserModelFactory {
             gender: signUpModel.gender,
             bio: signUpModel.bio)
     }
+
+    func construct(from model: SignUpRawModel) -> SignUpUser? {
+        guard
+            let password = model.password,
+            let name = model.name,
+            let lastname = model.lastname,
+            let username = model.username,
+            let email = model.email,
+            let cardNumber = model.creditCard,
+            let genderString = model.gender,
+            let gender = Gender(rawValue: genderString),
+            let bio = model.bio
+        else {
+            return nil
+        }
+
+        return SignUpUser(
+            name: name,
+            lastname: lastname,
+            username: username,
+            password: password,
+            email: email,
+            creditCard: cardNumber,
+            gender: gender,
+            bio: bio)
+    }
 }
