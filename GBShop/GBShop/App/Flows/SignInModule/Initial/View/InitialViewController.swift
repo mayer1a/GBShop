b219ccc4901fc93294775644fb2ba1376effeebb
Artem V. Mayer <mayer1art@gmail.com>
2023-03-17 13:48:26 +0700

    add separated InitialView using

--- GBShop/GBShop/App/Flows/SignInModule/Initial/View/InitialViewController.swift
+++ GBShop/GBShop/App/Flows/SignInModule/Initial/View/InitialViewController.swift
@@ -12,26 +12,16 @@ final class InitialViewController: UIViewController {
     // MARK: - Private properties
 
     private var presenter: InitialPresenterProtocol!
-    private let logoImage: UIImageView
-    private let loadingSpinner: UIActivityIndicatorView
-    private let imageSize: CGFloat
 
-    // MARK: - Constructions
-
-    required init() {
-        logoImage = UIImageView()
-        loadingSpinner = UIActivityIndicatorView(style: .large)
-        imageSize = 64.0
-
-        super.init(nibName: nil, bundle: nil)
-        configureViewComponents()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private var initialView: InitialView? {
+        isViewLoaded ? view as? InitialView : nil
     }
 
     // MARK: - Lifecycle
+
+    override func loadView() {
+        view = InitialView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,11 +33,6 @@ final class InitialViewController: UIViewController {
         presenter.onViewWillAppear()
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        showNavigationBar()
-    }
-
     // MARK: - Functions
 
     func setPresenter(_ presenter: InitialPresenterProtocol) {
@@ -56,46 +41,8 @@ final class InitialViewController: UIViewController {
 
     // MARK: - Private functions
 
-    private func configureViewComponents() {
-        view.backgroundColor = .systemBackground
-        view.addSubview(logoImage)
-        view.addSubview(loadingSpinner)
-
-        configureLogo()
-        configureSpinner()
-    }
-
-    private func configureLogo() {
-        logoImage.backgroundColor = .systemBackground
-        logoImage.contentMode = .scaleAspectFit
-        logoImage.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            logoImage.widthAnchor.constraint(equalToConstant: imageSize),
-            logoImage.heightAnchor.constraint(equalTo: logoImage.widthAnchor),
-            logoImage.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            logoImage.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
-        ])
-    }
-
-    private func configureSpinner() {
-        loadingSpinner.hidesWhenStopped = true
-        loadingSpinner.color = .label
-        loadingSpinner.tintColor = .label
-        loadingSpinner.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            loadingSpinner.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 20),
-            loadingSpinner.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
-        ])
-    }
-
     private func hideNavigationBar() {
         navigationController?.setNavigationBarHidden(true, animated: false)
-    }
-
-    private func showNavigationBar() {
-        navigationController?.setNavigationBarHidden(false, animated: false)
     }
 }
 
@@ -106,10 +53,10 @@ extension InitialViewController: InitialViewProtocol {
     // MARK: - Functions
 
     func showLoadingSpinner() {
-        loadingSpinner.startAnimating()
+        initialView?.loadingSpinner.startAnimating()
     }
 
     func hideLoadingSpinner() {
-        loadingSpinner.stopAnimating()
+        initialView?.loadingSpinner.stopAnimating()
     }
 }
