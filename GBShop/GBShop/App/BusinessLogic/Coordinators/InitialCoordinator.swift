45dd5ff25afced332f4a144e201f419dcc9fd03f
Artem V. Mayer <mayer1art@gmail.com>
2023-03-23 15:19:48 +0700

    add InitialCoordinator

---
+++ GBShop/GBShop/App/BusinessLogic/Coordinators/InitialCoordinator.swift
@@ -0,0 +1,73 @@
+//
+//  InitialCoordinator.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 23.03.2023.
+//
+
+import UIKit
+
+// MARK: - InitialBaseCoordinator
+
+final class InitialCoordinator: InitialBaseCoordinator {
+
+    // MARK: - Properties
+
+    var parentCoordinator: MainBaseCoordinator?
+    var assemblyBuilder: ModuleBuilderProtocol?
+    lazy var rootViewController: UIViewController = UIViewController()
+
+    // MARK: - Constructions
+
+    init(_ parentCoordinator: MainBaseCoordinator?, assemblyBuilder: ModuleBuilderProtocol?) {
+        self.parentCoordinator = parentCoordinator
+        self.assemblyBuilder = assemblyBuilder
+    }
+
+    // MARK: - Functions
+
+    func start() -> UIViewController {
+        let initialViewController = ModuleBuilder().createInitialModule(coordinator: self)
+        rootViewController = UINavigationController(rootViewController: initialViewController)
+        return rootViewController
+    }
+
+    func goToCatalog() {
+        parentCoordinator?.moveTo(flow: .tabBar(.catalogFlow(.catalogScreen)), userData: nil)
+    }
+
+    func moveTo(flow: AppFlow, userData: [UserDataKey : Any]?) {
+        switch flow {
+        case .initial(let initialFlow):
+            handleInitialFlow(for: initialFlow)
+        default:
+            parentCoordinator?.moveTo(flow: flow, userData: userData)
+        }
+    }
+
+    // MARK: - Private functions
+
+    private func handleInitialFlow(for screen: InitialFlow) {
+        switch screen {
+        case .initialScreen:
+            navigationRootViewController?.popToRootViewController(animated: true) // ???
+        case .signInScreen:
+            goToSignInScreen()
+        case .signUpScreen:
+            goToSignUpScreen()
+        }
+    }
+
+    private func goToSignInScreen() {
+        guard let signInViewController = assemblyBuilder?.createSignInModule(coordinator: self) else { return }
+        navigationRootViewController?.setViewControllers([signInViewController], animated: true)
+        navigationRootViewController?.setNavigationBarHidden(true, animated: true)
+    }
+
+    private func goToSignUpScreen() {
+        guard let signUpViewController = assemblyBuilder?.createSignUpModule(coordinator: self) else { return }
+        signUpViewController.title = "Регистрация"
+        navigationRootViewController?.pushViewController(signUpViewController, animated: true)
+        navigationRootViewController?.setNavigationBarHidden(false, animated: true)
+    }
+}
