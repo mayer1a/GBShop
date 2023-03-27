2cfe1b5d6f99db82f8d51e416a7c102a08047b7a
Artem V. Mayer <mayer1art@gmail.com>
2023-03-28 05:11:08 +0700

    add ProductViewModel for ProductModule

---
+++ GBShop/GBShop/App/Entity/ProductViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProductViewModel.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 28.03.2023.
+//
+
+import Foundation
+
+struct ProductViewModel {
+    let category: String
+    let name: String
+    let price: String
+    let description: String
+}
