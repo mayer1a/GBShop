97d206c3179ef57c76a569bfb176940dea45ca6d
Artem V. Mayer <mayer1art@gmail.com>
2023-03-28 07:45:53 +0700

    add ReviewCellModel

---
+++ GBShop/GBShop/App/Entity/ReviewCellModel.swift
@@ -0,0 +1,15 @@
+//
+//  ReviewCellModel.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 28.03.2023.
+//
+
+import Foundation
+
+struct ReviewCellModel {
+    let userId: String
+    let body: String
+    let reviewStars: Int
+    let date: String
+}
