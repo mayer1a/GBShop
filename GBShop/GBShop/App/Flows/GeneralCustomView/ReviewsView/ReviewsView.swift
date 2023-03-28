c448341d856728e399ee114b3a171e0b83a06781
Artem V. Mayer <mayer1art@gmail.com>
2023-03-28 08:08:34 +0700

    add ReviewsView

---
+++ GBShop/GBShop/App/Flows/GeneralCustomView/ReviewsView/ReviewsView.swift
@@ -0,0 +1,54 @@
+//
+//  ReviewsView.swift
+//  GBShop
+//
+//  Created by Artem Mayer on 28.03.2023.
+//
+
+import UIKit
+
+final class ReviewsView: UIView {
+
+    // MARK: - Properties
+
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.separatorColor = .white
+        tableView.separatorStyle = .none
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(ReviewsViewCell.self, forCellReuseIdentifier: ReviewsViewCell.cellIdentifier)
+
+        return tableView
+    }()
+
+    // MARK: - Constructions
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureComponents()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureComponents()
+    }
+
+    // MARK: - Private functions
+
+    private func configureComponents() {
+        backgroundColor = .white
+
+        addSubview(tableView)
+        addCollectionViewConstraints()
+    }
+
+    private func addCollectionViewConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
