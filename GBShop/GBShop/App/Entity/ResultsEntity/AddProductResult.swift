fbfeff143d7a995e08f872a9ef35c701cb707a0e
Artem V. Mayer <mayer1art@gmail.com>
2023-03-31 03:31:25 +0700

    add errorMessage property and CodingKeys to AddProductResult, PayBasketResult and RemoveProductResult

--- GBShop/GBShop/App/Entity/ResultsEntity/AddProductResult.swift
+++ GBShop/GBShop/App/Entity/ResultsEntity/AddProductResult.swift
@@ -10,7 +10,15 @@ import Foundation
 /// The data model of the server's response to a request to add an item to the user's basket.
 struct AddProductResult: Codable {
 
+    // MARK: - CodingKeys
+
+    enum CodingKeys: String, CodingKey {
+        case result
+        case errorMessage = "error_message"
+    }
+
     // MARK: - Properties
 
     let result: Int
+    let errorMessage: String?
 }
