dbb7e53e916996292671174846043069a600cd44
Artem V. Mayer <mayer1art@gmail.com>
2023-03-31 11:07:49 +0700

    replace indexes instance from [Int] to [IndexPath], fix bug in productQuantityDidChange method

--- GBShop/GBShop/App/Flows/BasketModule/Presenter/BasketPresenter.swift
+++ GBShop/GBShop/App/Flows/BasketModule/Presenter/BasketPresenter.swift
@@ -11,10 +11,11 @@ protocol BasketViewProtocol: AnyObject {
     func showFailure(with message: String?)
     func removeWarning()
     func basketDidFetch(_ basket: BasketModel)
-    func insertProductRows(at indexes: [Int], basket: BasketModel)
-    func updateProductRows(at indexes: [Int], basket: BasketModel)
-    func deleteProductRows(at indexes: [Int], basket: BasketModel)
+    func insertProductRows(at indexes: [IndexPath], basket: BasketModel)
+    func updateProductRows(at indexes: [IndexPath], basket: BasketModel)
+    func deleteProductRows(at indexes: [IndexPath], basket: BasketModel)
     func basketDidPay()
+    func setDisabledWhileSending(at index: Int?)
 }
 
 protocol BasketPresenterProtocol: AnyObject {
@@ -23,28 +24,28 @@ protocol BasketPresenterProtocol: AnyObject {
         requestFactory: BasketRequestFactory,
         coordinator: BasketBaseCoordinator,
         userId: Int)
-
+    
     func onViewWillAppear()
     func placeOrderButtonDidTap()
-    func productQuantityDidChange(_ product: BasketCellModel)
+    func productQuantityDidChange(_ product: BasketCellModel, quantity: Int, index: Int)
     func getImage(from link: String, completion: @escaping (UIImage?) -> Void)
 }
 
 // MARK: - BasketPresenter
 
 final class BasketPresenter {
-
+    
     // MARK: - Private properties
-
+    
     private weak var view: BasketViewProtocol!
     private let coordinator: BasketBaseCoordinator
     private let requestFactory: BasketRequestFactory
     private var imageDownloader: ImageDownloaderProtocol!
     private var userId: Int
     private var basket: BasketModel?
-
+    
     // MARK: - Constructions
-
+    
     init(
         view: BasketViewProtocol,
         requestFactory: BasketRequestFactory,
@@ -56,28 +57,28 @@ final class BasketPresenter {
         self.coordinator = coordinator
         self.userId = userId
     }
-
+    
     // MARK: Functions
-
+    
     func setupDownloader(_ imageDownloader: ImageDownloaderProtocol) {
         self.imageDownloader = imageDownloader
     }
-
+    
     // MARK: - Private functions
-
-    private func handleResult(result: GetBasketResult, _ action: ([Int], BasketModel) -> Void) {
+    
+    private func handleResult(result: GetBasketResult, _ action: ([IndexPath], BasketModel) -> Void) {
         guard result.result != 0, let basket = result.basket else {
             view.showFailure(with: result.errorMessage)
             return
         }
-
+        
         let userBasket = BasketCellModelFactory.construct(from: basket)
         let indexes = getIndexes(for: userBasket, with: self.basket)
         self.basket = userBasket
-
+        
         action(indexes, userBasket)
     }
-
+    
     private func basketDidFetch(_ response: AFBasketResult) {
         switch response.result {
         case .success(let basketResult):
@@ -85,7 +86,7 @@ final class BasketPresenter {
                 self.view.showFailure(with: basketResult.errorMessage)
                 return
             }
-
+            
             let basketModel = BasketCellModelFactory.construct(from: userBasket)
             basket = basketModel
             view.basketDidFetch(basketModel)
@@ -93,7 +94,7 @@ final class BasketPresenter {
             self.view.showFailure(with: "Ошибка сервера. Повторите попытку позже.")
         }
     }
-
+    
     private func handleAddProductResponse(_ response: AFBasketResult) {
         switch response.result {
         case .success(let basketResult):
@@ -102,7 +103,7 @@ final class BasketPresenter {
             view.showFailure(with: "Ошибка сервера. Повторите попытку позже.")
         }
     }
-
+    
     private func handleEditProductResponse(_ response: AFBasketResult) {
         switch response.result {
         case .success(let basketResult):
@@ -111,7 +112,7 @@ final class BasketPresenter {
             view.showFailure(with: "Ошибка сервера. Повторите попытку позже.")
         }
     }
-
+    
     private func handleRemoveProductResponse(_ response: AFBasketResult) {
         switch response.result {
         case .success(let basketResult):
@@ -120,7 +121,7 @@ final class BasketPresenter {
             view.showFailure(with: "Ошибка сервера. Повторите попытку позже.")
         }
     }
-
+    
     private func handlePayBasketResponse(_ response: AFPayBasketResult) {
         switch response.result {
         case .success(let basketResult):
@@ -128,62 +129,60 @@ final class BasketPresenter {
                 view.showFailure(with: basketResult.errorMessage)
                 return
             }
-
+            
             basket = nil
             view.basketDidPay()
         case .failure(_):
             view.showFailure(with: "Ошибка сервера. Повторите попытку позже.")
         }
     }
-
+    
     private func fetchBasket() {
         requestFactory.getBasket(userId: userId) { [weak self] response in
             guard let self else { return }
-
+            
             DispatchQueue.main.async {
                 self.basketDidFetch(response)
             }
         }
     }
-
+    
     private func addProduct(_ product: BasketCellModel) {
         guard let basketElement = BasketCellModelFactory.construct(from: product) else { return }
-
+        
         requestFactory.addProduct(userId: userId, basketElement: basketElement) { [weak self] response in
             guard let self else { return }
-
+            
             DispatchQueue.main.async {
                 self.handleAddProductResponse(response)
             }
         }
     }
-
-    private func editProduct(_ product: BasketCellModel) {
-        guard let basketElement = BasketCellModelFactory.construct(from: product) else { return }
-
+    
+    private func editProduct(_ basketElement: BasketElement) {
         requestFactory.editProduct(userId: userId, basketElement: basketElement) { [weak self] response in
             guard let self else { return }
-
+            
             DispatchQueue.main.async {
                 self.handleEditProductResponse(response)
             }
         }
     }
-
-    private func removeProduct(_ product: BasketCellModel) {
-        requestFactory.removeProduct(userId: userId, productId: product.productId) { [weak self] response in
+    
+    private func removeProduct(_ basketElement: BasketElement) {
+        requestFactory.removeProduct(userId: userId, productId: basketElement.product.id) { [weak self] response in
             guard let self else { return }
-
+            
             DispatchQueue.main.async {
                 self.handleRemoveProductResponse(response)
             }
         }
     }
-
+    
     private func payBasket() {
         requestFactory.payBasket(userId: userId) { [weak self] response in
             guard let self else { return }
-
+            
             DispatchQueue.main.async {
                 self.handlePayBasketResponse(response)
             }
@@ -194,29 +193,35 @@ final class BasketPresenter {
 // MARK: - Extensions
 
 extension BasketPresenter: BasketPresenterProtocol {
-
+    
     // MARK: - Functions
-
+    
     func onViewWillAppear() {
+        view.setDisabledWhileSending(at: nil)
         fetchBasket()
     }
-
+    
     func placeOrderButtonDidTap() {
         // TODO: add a call to the coordinator method to go to the screen for filling in the data for the purchase
+        view.setDisabledWhileSending(at: nil)
         payBasket()
     }
-
-    func productQuantityDidChange(_ product: BasketCellModel) {
-        if product.quantity == 0 {
-            removeProduct(product)
+    
+    func productQuantityDidChange(_ product: BasketCellModel, quantity: Int, index: Int) {
+        guard let updateProduct = BasketCellModelFactory.construct(from: product, with: quantity) else { return }
+        
+        view.setDisabledWhileSending(at: index)
+        
+        if quantity == 0 {
+            removeProduct(updateProduct)
         } else {
-            editProduct(product)
+            editProduct(updateProduct)
         }
     }
-
+    
     func getImage(from link: String, completion: @escaping (UIImage?) -> Void) {
         guard let url = URL(string: link) else { return }
-
+        
         imageDownloader.getImage(fromUrl: url) { (image, _) in
             completion(image)
         }
