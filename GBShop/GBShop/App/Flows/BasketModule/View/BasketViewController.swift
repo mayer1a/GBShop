d7448394332b5231889819bfe4edd85a4d574edd
Artem V. Mayer <mayer1art@gmail.com>
2023-03-31 11:03:37 +0700

    add reloadTable, setEnabledView, insert-, update-, deleteRows and disable element methods. Add extension for setup title by scroll offset

--- GBShop/GBShop/App/Flows/BasketModule/View/BasketViewController.swift
+++ GBShop/GBShop/App/Flows/BasketModule/View/BasketViewController.swift
@@ -18,8 +18,10 @@ final class BasketViewController: UIViewController {
     // MARK: - Private properties
 
     private var presenter: BasketPresenterProtocol!
-    private var basket: BasketModel?
     private var quantityAction: ((Int) -> Void)?
+    private var basket: BasketModel? {
+        didSet { setupTitle() }
+    }
 
     // MARK: - Lifecycle
 
@@ -29,7 +31,7 @@ final class BasketViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTargets()
+        setupComponents()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -45,21 +47,38 @@ final class BasketViewController: UIViewController {
 
     // MARK: - Private functions
 
-    private func setupTargets() {
+    private func setupComponents() {
         basketView?.tableView.delegate = self
         basketView?.tableView.dataSource = self
         basketView?.placeOrderButton.addTarget(self, action: #selector(placeOrderButtonDidTap), for: .touchUpInside)
+        navigationItem.largeTitleDisplayMode = .never
     }
 
     private func stepperAction(indexPath: IndexPath) -> ((Int) -> Void)? {
-        quantityAction = { [weak self] _ in
+        quantityAction = { [weak self] value in
             guard let self, let cellModel = self.basket?.cellModels[indexPath.row] else {
                 return
             }
 
-            self.presenter.productQuantityDidChange(cellModel)
+            self.presenter.productQuantityDidChange(cellModel, quantity: value, index: indexPath.row)
         }
+
         return quantityAction
+    }
+
+    private func reloadTable(indexes: [IndexPath], with animation: UITableView.RowAnimation, _ completion: ReloadCompletion) {
+        basketView?.tableView.beginUpdates()
+        completion?(indexes, animation)
+        basketView?.tableView.endUpdates()
+        setEnabledView()
+    }
+
+    private func setEnabledView() {
+        guard let basket else { return }
+
+        basketView?.placeOrderButton.isEnabled = true
+        basketView?.isHiddenBottomView = basket.productsQuantity < 1
+        basketView?.setupPrice(basket.amount)
     }
 
     @objc private func placeOrderButtonDidTap(_ sender: UIButton) {
@@ -75,6 +94,7 @@ extension BasketViewController: BasketViewProtocol {
     
     func showFailure(with message: String?) {
         // TODO: call the alert display method when it's ready
+        basketView?.isHiddenBottomView = basket == nil
     }
 
     func removeWarning() {
@@ -84,35 +104,35 @@ extension BasketViewController: BasketViewProtocol {
     func basketDidFetch(_ basket: BasketModel) {
         self.basket = basket
         basketView?.tableView.reloadData()
+        setEnabledView()
     }
 
-    func insertProductRows(at indexes: [Int], basket: BasketModel) {
+    func insertProductRows(at indexes: [IndexPath], basket: BasketModel) {
         self.basket = basket
-        let indexPath = indexes.compactMap { index -> IndexPath in IndexPath(row: index, section: 0) }
-        basketView?.tableView.beginUpdates()
-        basketView?.tableView.insertRows(at: indexPath, with: .automatic)
-        basketView?.tableView.endUpdates()
+        reloadTable(indexes: indexes, with: .automatic, basketView?.tableView.insertRows)
     }
 
-    func updateProductRows(at indexes: [Int], basket: BasketModel) {
+    func updateProductRows(at indexes: [IndexPath], basket: BasketModel) {
         self.basket = basket
-        let indexPath = indexes.compactMap { index -> IndexPath in IndexPath(row: index, section: 0) }
-        basketView?.tableView.beginUpdates()
-        basketView?.tableView.reloadRows(at: indexPath, with: .automatic)
-        basketView?.tableView.endUpdates()
+        reloadTable(indexes: indexes, with: .none, basketView?.tableView.reloadRows)
     }
 
-    func deleteProductRows(at indexes: [Int], basket: BasketModel) {
+    func deleteProductRows(at indexes: [IndexPath], basket: BasketModel) {
         self.basket = basket
-        let indexPath = indexes.compactMap { index -> IndexPath in IndexPath(row: index, section: 0) }
-        basketView?.tableView.beginUpdates()
-        basketView?.tableView.deleteRows(at: indexPath, with: .automatic)
-        basketView?.tableView.endUpdates()
+        reloadTable(indexes: indexes, with: .automatic, basketView?.tableView.deleteRows)
     }
 
     func basketDidPay() {
         basket = nil
         basketView?.tableView.reloadData()
+    }
+
+    func setDisabledWhileSending(at index: Int?) {
+        guard let index else { return }
+
+        let cell = basketView?.tableView.cellForRow(at: IndexPath(row: index, section: 0)) as? BasketProductCell
+        cell?.quantityStepper.isEnabled = false
+        basketView?.placeOrderButton.isEnabled = false
     }
 }
 
@@ -129,6 +149,7 @@ extension BasketViewController: UITableViewDataSource {
             return UITableViewCell()
         }
 
+        basketCell.resetData()
         presenter.getImage(from: cellModels[indexPath.row].imageUrl) { image in
             basketCell.setupImage(image)
         }
@@ -137,10 +158,6 @@ extension BasketViewController: UITableViewDataSource {
         basketCell.quantityStepper.stepperAction = stepperAction(indexPath: indexPath)
 
         return basketCell
-    }
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -157,13 +174,13 @@ extension BasketViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: BasketHeaderView.reuseIdentifier)
 
-        guard let basketHeaderView = headerView as? BasketHeaderView, let quantity = basket?.productsQuantity else {
-            return nil
+        if let quantity = basket?.productsQuantity, quantity > 0 {
+            (headerView as? BasketHeaderView)?.setupQuantity(quantity)
+        } else {
+            (headerView as? BasketHeaderView)?.setupEmpty()
         }
 
-        basketHeaderView.setupQuantity(quantity)
-
-        return basketHeaderView
+        return headerView
     }
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -172,5 +189,57 @@ extension BasketViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
         ProductConstants.buttonsHeight
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let headerHeight = basketView?.tableView.headerView(forSection: 0)?.bounds.height else {
+            return
+        }
+
+        let titleHeight = headerHeight - ProductConstants.sideIndent
+        let scrollOffset = scrollView.contentOffset.y + view.safeAreaInsets.top
+
+        if scrollOffset > titleHeight {
+            setupScreenTitile()
+        } else {
+            setupHeaderTitle()
+        }
+    }
+}
+
+// MARK: - Setup titles extension
+
+extension BasketViewController {
+
+    // MARK: - Private functions
+
+    private func setupTitle() {
+        if navigationItem.title != nil && navigationItem.title != "" {
+            setupScreenTitile()
+        } else {
+            setupHeaderTitle()
+        }
+    }
+
+    private func setupScreenTitile() {
+        guard let value = basket?.productsQuantity, value > 0 else {
+            navigationItem.title = "в вашей корзине пока ничего нет ..."
+            (basketView?.tableView.headerView(forSection: 0) as? BasketHeaderView)?.setupEmpty()
+            return
+        }
+
+        navigationItem.title = "корзина / \(value) шт."
+        (basketView?.tableView.headerView(forSection: 0) as? BasketHeaderView)?.setupQuantity(value)
+    }
+
+    private func setupHeaderTitle() {
+        guard let value = basket?.productsQuantity, value > 0 else {
+            (basketView?.tableView.headerView(forSection: 0) as? BasketHeaderView)?.setupEmpty()
+            navigationItem.title = ""
+            return
+        }
+
+        (basketView?.tableView.headerView(forSection: 0) as? BasketHeaderView)?.setupQuantity(value)
+        navigationItem.title = ""
     }
 }
